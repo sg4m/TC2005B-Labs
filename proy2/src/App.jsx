69d182407f8b3d86862ea0bef099250095696bcf
sg4m <A01753159@itesm.mx>
2025-09-10 20:24:54 -0600

    feat: add Material-UI components and update App layout
    - Added Material-UI dependencies: @emotion/styled, @mui/icons-material, @mui/material
    - Replaced existing buttons and layout in App component with Material-UI Button, TextField, ButtonGroup, Checkbox, and Rating components
    - Cleaned up SimpleForm component by removing unnecessary whitespace

--- proy2/src/App.jsx
+++ proy2/src/App.jsx
@@ -3,37 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Button, TextField, ButtonGroup, Checkbox, Rating } from '@mui/material';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increase = {count}
-        </button>
-        <button onClick ={() => setCount((count) => count - 1)}>
-          Decrease = {count}
-        </button>
-        <button onClick={() => setCount(0)}>
-          Reset
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <TextField
+  hiddenLabel
+  id="filled-hidden-label-small"
+  defaultValue="Small"
+  variant="filled"
+  size="small"
+/>
+<TextField
+  hiddenLabel
+  id="filled-hidden-label-normal"
+  defaultValue="Normal"
+  variant="filled"
+/>
+
+<ButtonGroup variant="contained" aria-label="Basic button group">
+  <Button>One</Button>
+  <Button>Two</Button>
+  <Button>Three</Button>
+</ButtonGroup>
+
+<Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+<Checkbox
+  {...label}
+  icon={<BookmarkBorderIcon />}
+  checkedIcon={<BookmarkIcon />}
+/>
+
+<Rating name="half-rating" defaultValue={2.5} precision={0.5} />
     </>
   )
 }
--- proy2/src/SimpleForm.jsx
+++ proy2/src/SimpleForm.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { useState } from 'react';
 
 
-
-
-
 const SimpleForm = () => {
   return (
     <div>
